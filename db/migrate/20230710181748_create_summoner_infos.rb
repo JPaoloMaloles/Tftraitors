7f50d7c76525531117fc6159fe3cb7e1988831ea
Paolo Maloles <jpaolomaloles@gmail.com>
2023-07-10 13:33:25 -0500

    created and migrated SummonerInfo model

---
+++ db/migrate/20230710181748_create_summoner_infos.rb
@@ -0,0 +1,16 @@
+class CreateSummonerInfos < ActiveRecord::Migration[7.0]
+  def change
+    create_table :summoner_infos do |t|
+      t.string :puuid
+      t.string :tier
+      t.string :rank
+      t.integer :league_points
+      t.string :summoner_id
+      t.string :summoner_name
+      t.integer :wins
+      t.integer :losses
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_10_163154) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_10_181748) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "summoner_infos", force: :cascade do |t|
+    t.string "puuid"
+    t.string "tier"
+    t.string "rank"
+    t.integer "league_points"
+    t.string "summoner_id"
+    t.string "summoner_name"
+    t.integer "wins"
+    t.integer "losses"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"

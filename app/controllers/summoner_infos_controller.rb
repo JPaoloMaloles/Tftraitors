301ee7675465eaeaf5733b9da2dd53dcfc7400e9
Paolo Maloles <jpaolomaloles@gmail.com>
2023-07-10 14:03:52 -0500

    fixed update action name, temporarily changed routes to use resources for SummonerInfo, added authorization to summoner_infos controller

--- app/controllers/summoner_infos_controller.rb
+++ app/controllers/summoner_infos_controller.rb
@@ -1,4 +1,6 @@
 class SummonerInfosController < ApplicationController
+  before_action :authenticate_user, except: [:index, :show]
+
   def show
     @summoner_info = SummonerInfo.find_by(id: params["id"])
     render :show
@@ -7,5 +9,40 @@ class SummonerInfosController < ApplicationController
   def index
     @summoner_infos = SummonerInfo.all
     render :index
+  end
+
+  def create
+    @summoner_info = SummonerInfo.create(
+      puuid: params["puuid"],
+      tier: params["tier"],
+      rank: params["rank"],
+      league_points: params["league_points"],
+      summoner_id: params["summoner_id"],
+      summoner_name: params["summoner_name"],
+      wins: params["wins"],
+      losses: params["losses"],
+    )
+    render :show
+  end
+
+  def update
+    @summoner_info = SummonerInfo.find_by(id: params["id"])
+    @summoner_info.update(
+      puuid: params["puuid"] || @summoner_info.puuid,
+      tier: params["tier"] || @summoner_info.tier,
+      rank: params["rank"] || @summoner_info.rank,
+      league_points: params["league_points"] || @summoner_info.league_points,
+      summoner_id: params["summoner_id"] || @summoner_info.summoner_id,
+      summoner_name: params["summoner_name"] || @summoner_info.summoner_name,
+      wins: params["wins"] || @summoner_info.wins,
+      losses: params["losses"] || @summoner_info.losses,
+    )
+    render :show
+  end
+
+  def destroy
+    summoner_info = SummonerInfo.find_by(id: params["id"])
+    summoner_info.destroy
+    render json: { message: "Summoner Info was Destroyed" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
 
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
-  get "/summoner_infos" => "summoner_infos#index"
-  get "/summoner_infos/:id" => "summoner_infos#show"
+  # get "/summoner_infos" => "summoner_infos#index"
+  # get "/summoner_infos/:id" => "summoner_infos#show"
   # get "/summoner_infos", controller: "summoner_infos", action: "index"
+  resources :summoner_infos
 end

3cd5d5084afe000fa2841c38c8b438184c790121
Paolo Maloles <jpaolomaloles@gmail.com>
2023-07-11 13:09:43 -0500

    coded create, update, destroy action of MatchSummonerPerformance. Added authorization to match_summoner_performances controller

--- app/controllers/match_summoner_performances_controller.rb
+++ app/controllers/match_summoner_performances_controller.rb
@@ -1,4 +1,6 @@
 class MatchSummonerPerformancesController < ApplicationController
+  before_action :authenticate_user, except: [:index, :show]
+
   def show
     @match_summoner_performance = MatchSummonerPerformance.find_by(id: params["id"])
     render :show
@@ -10,11 +12,41 @@ class MatchSummonerPerformancesController < ApplicationController
   end
 
   def create
+    @match_summoner_performance = MatchSummonerPerformance.create(
+      match_id: params["match_id"],
+      summoner_info_id: params["summoner_info_id"],
+      riot_match_id: params["riot_match_id"],
+      puuid: params["puuid"],
+      gold_left: params["gold_left"],
+      last_round: params["last_round"],
+      level_placement: params["level_placement"],
+      players_eliminated: params["players_eliminated"],
+      time_eliminated: params["time_eliminated"],
+      total_damage_to_players: params["total_damage_to_players"],
+    )
+    render :show
   end
 
   def update
+    @match_summoner_performance = MatchSummonerPerformance.find_by(id: params["id"])
+    @match_summoner_performance.update(
+      match_id: params["match_id"] || @match_summoner_performance.match_id,
+      summoner_info_id: params["summoner_info_id"] || @match_summoner_performance.summoner_info_id,
+      riot_match_id: params["riot_match_id"] || @match_summoner_performance.riot_match_id,
+      puuid: params["puuid"] || @match_summoner_performance.puuid,
+      gold_left: params["gold_left"] || @match_summoner_performance.gold_left,
+      last_round: params["last_round"] || @match_summoner_performance.last_round,
+      level_placement: params["level_placement"] || @match_summoner_performance.level_placement,
+      players_eliminated: params["players_eliminated"] || @match_summoner_performance.players_eliminated,
+      time_eliminated: params["time_eliminated"] || @match_summoner_performance.time_eliminated,
+      total_damage_to_players: params["total_damage_to_players"] || @match_summoner_performance.total_damage_to_players,
+    )
+    render :show
   end
 
   def destroy
+    match_summoner_performance = MatchSummonerPerformance.find_by(id: params["id"])
+    match_summoner_performance.destroy
+    render json: { message: "MatchSummonerPerformance has been destroyed" }
   end
 end

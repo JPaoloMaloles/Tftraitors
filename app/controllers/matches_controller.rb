2f9d56f01b6cd80087fff2064914c6dad627461e
Paolo Maloles <jpaolomaloles@gmail.com>
2023-07-14 22:49:59 -0500

    included summonerInfo_id in json render

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -62,7 +62,7 @@ class MatchesController < ApplicationController
     pp matches
     puts
 
-    render json: { riot_match_ids: matches }
+    render json: { riot_match_ids: matches, summonerInfo_id: params["summonerInfo_id"] }
 
     # #returns
     # :an_unnamed_list_of_strings

c83452becfb21269ec530be72d9ada6a4f9500d2
Paolo Maloles <jpaolomaloles@gmail.com>
2023-07-15 21:28:32 -0500

    successfully rendering riot_fourth

--- app/controllers/match_summoner_performances_controller.rb
+++ app/controllers/match_summoner_performances_controller.rb
@@ -60,20 +60,25 @@ class MatchSummonerPerformancesController < ApplicationController
 
   def riot_fourth
     require "http"
-    p "tftRegion: #{params["params"]["tftRegion"]}"
-    p "riot_match_id: #{params["params"]["riot_match_id"]}"
-    p "summonerInfo_id: #{params["params"]["summonerInfo_id"]}"
-    p "match_id: #{params["params"]["match_id"]}"
+    # p "tftRegion: #{params["params"]["tftRegion"]}"
+    # p "riot_match_id: #{params["params"]["riot_match_id"]}"
+    # p "summonerInfo_id: #{params["params"]["summonerInfo_id"]}"
+    # p "match_id: #{params["params"]["match_id"]}"
 
     single_match = false
 
-    api_data = HTTP.get("https://#{params["params"]["tftRegion"]}.api.riotgames.com/tft/match/v1/matches/#{params["params"]["riot_match_id"]}?api_key=#{ENV["RIOT_API_KEY"]}")
+    # api_data = HTTP.get("https://#{params["params"]["tftRegion"]}.api.riotgames.com/tft/match/v1/matches/#{params["params"]["riot_match_id"]}?api_key=#{ENV["RIOT_API_KEY"]}")
+    api_data = HTTP.get("https://americas.api.riotgames.com/tft/match/v1/matches/NA1_4702931242?api_key=#{ENV["RIOT_API_KEY"]}")
     single_match = api_data.parse(:json)
 
-    if single_match
+    test_match_id = 888
+    # single_match["info"]
+    # render json: single_match["info"]["participants"]
+
+    single_match["info"]["participants"].each do |participant_info|
       @match_summoner_performance = MatchSummonerPerformance.create(
-        match_id: params["params"]["match_id"],
-        summoner_info_id: params["params"]["summonerInfo_id"],
+        match_id: 1,
+        summoner_info_id: 1,
         riot_match_id: "a",
         puuid: "a",
         gold_left: "a",
@@ -87,9 +92,30 @@ class MatchSummonerPerformancesController < ApplicationController
         third_augment: "a",
         companion_id: "a",
       )
-      render :show
-    else
-      render json: { message: "unable to retrieve MatchSummonerPerformance" }
     end
+    @match_summoner_performances = MatchSummonerPerformance.where(match_id: 1)
+    render :index
+
+    # if single_match
+    # @match_summoner_performance = MatchSummonerPerformance.create(
+    #   match_id: params["params"]["match_id"],
+    #   summoner_info_id: params["params"]["summonerInfo_id"],
+    #   riot_match_id: "a",
+    #   puuid: "a",
+    #   gold_left: "a",
+    #   last_round: "a",
+    #   level_placement: "a",
+    #   players_eliminated: "a",
+    #   time_eliminated: "a",
+    #   total_damage_to_players: "a",
+    #   first_augment: "a",
+    #   second_augment: "a",
+    #   third_augment: "a",
+    #   companion_id: "a",
+    # )
+    # render :show
+    # else
+    #   render json: { message: "unable to retrieve MatchSummonerPerformance" }
+    # end
   end
 end

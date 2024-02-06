52ce574f9fc52a59e94f36be859931d47ec9d376
Paolo Maloles <jpaolomaloles@gmail.com>
2024-02-06 11:42:55 -0600

    coded create and update for traits and units controller

--- app/controllers/traits_controller.rb
+++ app/controllers/traits_controller.rb
@@ -10,14 +10,31 @@ class TraitsController < ApplicationController
   end
 
   def create
+    @trait = Trait.create(
+      name: params["name"],
+      num_units: params["num_units"],
+      style: params["style"],
+      tier_current: params["tier_current"],
+      tier_total: params["tier_total"],
+    )
+    render :show
   end
   
   def update
+    @trait = Trait.find_by(id: params["id"])
+    @trait.update(
+      name: params["name"] || @trait.name,
+      num_units: params["num_units"] || @trait.num_units,
+      style: params["style"] || @trait.style,
+      tier_current: params["tier_current"] || @trait.tier_current,
+      tier_total: params["tier_total"] || @trait.tier_total,
+    )
+    render :show
   end
 
   def destroy
-    trait = Trait.find_by(id: params["id"])
-    trait.destroy
+    @trait = Trait.find_by(id: params["id"])
+    @trait.destroy
     render json: {message: "trait has been destroyed"}
   end
 end
--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -10,9 +10,26 @@ class UnitsController < ApplicationController
   end
 
   def create
+    @unit = Unit.create(
+      character_id_name: params["character_id_name"],
+      item_names: params["item_names"],
+      name: params["name"],
+      rarity: params["rarity"],
+      tier: params["tier"],
+    )
+    render :show
   end
 
   def update
+    @unit = Unit.find_by(id: params["id"])
+    @unit.update(
+      character_id_name: params["character_id_name"] || @unit.character_id_name,
+      item_names: params["item_names"] || @unit.item_names,
+      name: params["name"] || @unit.name,
+      rarity: params["rarity"] || @unit.rarity,
+      tier: params["tier"] || @unit.tier,
+    )
+    render :show
   end
 
   def destroy

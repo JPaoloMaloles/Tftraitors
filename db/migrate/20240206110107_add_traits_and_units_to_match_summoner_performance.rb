da68310564b5b51385913d412ae56ab6adfd8ff5
Paolo Maloles <jpaolomaloles@gmail.com>
2024-02-06 05:38:53 -0600

    modified migration to be traits_id and units_id

--- db/migrate/20240206110107_add_traits_and_units_to_match_summoner_performance.rb
+++ db/migrate/20240206110107_add_traits_and_units_to_match_summoner_performance.rb
@@ -1,6 +1,6 @@
-class AddTraitsAndUnitsToMatchSummonerPerformance < ActiveRecord::Migration[7.0]
+class AddTraitsIdAndUnitsIdToMatchSummonerPerformance < ActiveRecord::Migration[7.0]
   def change
-    add_column :match_summoner_performances, :traits, :text, array: true, default: []
-    add_column :match_summoner_performances, :units, :text, array: true, default: []
+    add_column :match_summoner_performances, :traits_id, :integer
+    add_column :match_summoner_performances, :units_id, :integer
   end
 end

eb5b3bef292a6d7cb868a89bb2dd6ef6f2354be7
Paolo Maloles <jpaolomaloles@gmail.com>
2024-02-07 17:24:50 -0600

    added has_many relations relating to performance_traits and performance_units for SummonerInfo model

--- app/models/summoner_info.rb
+++ app/models/summoner_info.rb
@@ -1,4 +1,8 @@
 class SummonerInfo < ApplicationRecord
   has_many :match_summoner_performances
   has_many :matches, through: :match_summoner_performances
+  has_many :performance_traits, through: :match_summoner_performances
+  has_many :performance_units, through: :match_summoner_performances
+  has_many :traits, through: :performance_traits
+  has_many :units, through: :performance_units
 end
